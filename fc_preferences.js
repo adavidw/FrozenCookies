30639c1bdbb4dd427cf84df98f874411312ff06b
Aaron Wright <aawright@authorize.net>
2020-09-25 13:01:35 -0600

    pref formatting

--- fc_preferences.js
+++ fc_preferences.js
@@ -28,7 +28,7 @@ FrozenCookies.preferenceValues = {
     },
     'autoFortune':{
         'hint':'Automatically click the news ticker when Fortune News appears',
-        'display':["Auto Fortune OFF","Auto Fortune ON"],
+        'display':["Auto Fortune OFF", "Auto Fortune ON"],
         'default':0
     },
 
@@ -38,7 +38,7 @@ FrozenCookies.preferenceValues = {
     },
     'autoBuy':{
         'hint':'Automatically buy the most efficient building when you\'ve met its cost',
-        'display':["Autobuy OFF","Autobuy ON"],
+        'display':["Autobuy OFF", "Autobuy ON"],
         'default':0
     },
     'autoBulk':{
@@ -59,25 +59,25 @@ FrozenCookies.preferenceValues = {
  // is there a way to use cumulativeBuildingCost() to automatically figure an appropriate amount for these limits, maybe based on a specified number of minutes of cps?
     'cursorLimit':{
         'hint':'Limit max number of cursors to keep Godzamok useful',
-        'display':['Cursor Limit OFF','Cursor Limit ON'],
+        'display':['Cursor Limit OFF', 'Cursor Limit ON'],
         'default':0,
         'extras':'<a class="option" id="cursorMax" onclick="updateCursorMax(\'cursorMax\');">${cursorMax} cursors</a>'
     },
     'farmLimit':{
         'hint':'Limit max number of farms to keep Godzamok useful',
-        'display':['Farm Limit OFF','Farm Limit ON'],
+        'display':['Farm Limit OFF', 'Farm Limit ON'],
         'default':0,
         'extras':'<a class="option" id="farmMax" onclick="updateFarmMax(\'farmMax\');">${farmMax} farms</a>'
     },
     'towerLimit':{
         'hint':'Stop autobuying Wizard Towers at selected Max Mana, for spellcasting efficiency',
-        'display':['Wizard Tower Cap OFF','Wizard Tower Cap ON'],
+        'display':['Wizard Tower Cap OFF', 'Wizard Tower Cap ON'],
         'default':0,
         'extras':'<a class="option" id="manaMax" onclick="updateManaMax(\'manaMax\');">${manaMax} max Mana</a>'
     },
     'pastemode':{
         'hint':'Always autobuy the least efficient purchase. This is a stupid idea, you should never turn this on.',
-        'display':['Pastemode OFF','Pastemode ON'],
+        'display':['Pastemode OFF', 'Pastemode ON'],
         'default':0
     },
 
@@ -103,17 +103,17 @@ FrozenCookies.preferenceValues = {
     },
     'autoGS':{
         'hint':'Automatically turn on the Golden Switch during Dragonflight and Click Frenzy (and turn back off at the end)',
-        'display':['Auto-GS-Switch OFF','Auto-GS-Switch ON'],
+        'display':['Auto-GS-Switch OFF', 'Auto-GS-Switch ON'],
         'default':0
     },
     'autoGodzamok':{
         'hint':'Automatically sell all cursors and farms (except one) during Dragonflight and Click Frenzy if you worship Godzamok',
-        'display':['Auto-Godzamok OFF','Auto-Godzamok ON (Rebuy up to limit)','Auto-Godzamok ON (Rebuy all)'],
+        'display':['Auto-Godzamok OFF', 'Auto-Godzamok ON (Rebuy up to limit)', 'Auto-Godzamok ON (Rebuy all)'],
         'default':0
     },
     'autoSpell':{
         'hint':'Automatically cast selected spell when your mana is full',
-        'display':["Auto Cast OFF","Auto Cast CONJURE BAKED GOODS","Auto Cast FORCE THE HAND OF FATE","Auto Cast SPONTANEOUS EDIFICE","Auto Cast HAGGLER'S CHARM (cheapest)"],
+        'display':["Auto Cast OFF", "Auto Cast CONJURE BAKED GOODS", "Auto Cast FORCE THE HAND OF FATE", "Auto Cast SPONTANEOUS EDIFICE", "Auto Cast HAGGLER'S CHARM (cheapest)"],
         'default':0,
         'extras':'<a class="option" id="minCpSMult" onclick="updateCpSMultMin(\'minCpSMult\');">x${minCpSMult} minimum Frenzy</a>'
     },
@@ -124,12 +124,12 @@ FrozenCookies.preferenceValues = {
     },
     'numberDisplay':{
         'hint':'Change how numbers are shortened',
-        'display':["Number Display RAW","Number Display FULL (million, billion)","Number Display INITIALS (M, B)","Number Display SI UNITS (M, G, T)", "Number Display SCIENTIFIC (6.3e12)"],
+        'display':["Number Display RAW", "Number Display FULL (million, billion)", "Number Display INITIALS (M, B)", "Number Display SI UNITS (M, G, T)", "Number Display SCIENTIFIC (6.3e12)"],
         'default':1
     },
     'fancyui':{
         'hint':'Infobox type (can be slow)',
-        'display':['Infobox OFF','Infobox TEXT ONLY','Infobox WHEEL ONLY','Infobox WHEEL & TEXT'],
+        'display':['Infobox OFF', 'Infobox TEXT ONLY', 'Infobox WHEEL ONLY', 'Infobox WHEEL & TEXT'],
         'default':0
     },
 
@@ -145,31 +145,31 @@ FrozenCookies.preferenceValues = {
     },*/
     'holdSEBank':{
         'hint':'Maintain a bank for Spontaneous Edifice (already enabled if Auto Casting SE)',
-        'display':["SE Bank OFF","SE Bank ON"],
+        'display':["SE Bank OFF", "SE Bank ON"],
         'default':0
     },
     'setHarvestBankPlant':{
         'hint':'Maintain a bank for a specific plant you are going to harvest/let explode',
-        'display':['Harvesting Bank OFF','Harvesting Bank BAKEBERRY','Harvesting Bank CHOCOROOT','Harvesting Bank WHITE CHOCOROOT','Harvesting Bank QUEEENBEET','Harvesting Bank DUKETATER','Harvesting Bank CRUMBSPORE','Harvesting Bank DOUGHSHROOM'],
+        'display':['Harvesting Bank OFF', 'Harvesting Bank BAKEBERRY', 'Harvesting Bank CHOCOROOT', 'Harvesting Bank WHITE CHOCOROOT', 'Harvesting Bank QUEEENBEET', 'Harvesting Bank DUKETATER', 'Harvesting Bank CRUMBSPORE', 'Harvesting Bank DOUGHSHROOM'],
         'default':0
     },
     'setHarvestBankType':{
         'hint':'Calculate the needed harvesting bank based on whether a CPS multiplier is in effect when you intend to harvest (no effect if no plant was selected above).',
-        'display':['Harvesting during NO CPS MULTIPLER','Harvesting during FRENZY','Harvesting during BUILDING SPECIAL','Harvesting during FRENZY + BUILDING SPECIAL'],
+        'display':['Harvesting during NO CPS MULTIPLER', 'Harvesting during FRENZY', 'Harvesting during BUILDING SPECIAL', 'Harvesting during FRENZY + BUILDING SPECIAL'],
         'default':0,
         'extras':'<a class="option" id="maxSpecials" onclick="updateMaxSpecials(\'maxSpecials\');">${maxSpecials} Building specials</a>'
     },
 
 /*  'timeTravelMethod':{
         'hint':'Time travel is unstable. This determines how time travel works. If you\'re unsure, don\'t touch this.',
-        'display':['Time Travel DISABLED'],//,'Purchases by Estimated Effective CPS','Purchases by Simulated Real Time','Heavenly Chips by Estimated Effective CPS','Heavenly Chips by Simulated Real Time'],
+        'display':['Time Travel DISABLED'],//, 'Purchases by Estimated Effective CPS', 'Purchases by Simulated Real Time', 'Heavenly Chips by Estimated Effective CPS', 'Heavenly Chips by Simulated Real Time'],
         'default':0,
         'extras':'<a class="option" id="timeTravelPurchases" onclick="updateTimeTravelAmount();">Set Time Travel Amount</a>'
     },*/
 
     'simulatedGCPercent':{
         'hint':'Assume a percentage of Golden Cookies as "clicked" for GC efficiency calculations (100% recommended)',
-        'display':["0%","100%"],
+        'display':["0%", "100%"],
         'default':1
     },
     'fpsModifier':{
@@ -192,14 +192,14 @@ FrozenCookies.preferenceValues = {
     /*Doesnt work
     'showAchievements':{
         'hint':'Show achievement popups (Kind of broken early game)',
-        'display':['Achievement Popups OFF','Achievement Popups ON'],
+        'display':['Achievement Popups OFF', 'Achievement Popups ON'],
         'default':0
     },
     */
 
     'defaultSeason':{
         'hint':'Autobuy a selected season when no others have needed upgrades',
-        'display':['Default Season OFF','Default Season BUSINESS DAY','Default Season CHRISTMAS','Default Season EASTER','Default Season HALLOWEEN',"Default Season VALENTINE'S DAY"],
+        'display':['Default Season OFF', 'Default Season BUSINESS DAY', 'Default Season CHRISTMAS', 'Default Season EASTER', 'Default Season HALLOWEEN', "Default Season VALENTINE'S DAY"],
         'default':0
     }
 };
