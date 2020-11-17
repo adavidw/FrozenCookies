96eeeb582c8084d4ad33f6d657a71db80e75e2ab
Aaron Wright <aawright@authorize.net>
2020-11-16 23:15:43 -0700

    registerMod

--- fc_main.js
+++ fc_main.js
@@ -60,10 +60,9 @@ function registerMod() {    // register with the modding API
         save: saveFCData,
         load: setOverrides
     });
-    if (!FrozenCookies.loadedData){
+    if (!FrozenCookies.loadedData) {
         setOverrides();
     }
-    logEvent("Load", "Initial Load of Frozen Cookies v " + FrozenCookies.branch + "." + FrozenCookies.version + ". (You should only ever see this once.)");
 }
 
 
@@ -173,7 +172,8 @@ function registerMod() {    // register with the modding API
 
 
 
-function setOverrides(gameSaveData) {
+function setOverrides(gameSaveData) {   // load settings and initialize variables
+    logEvent("Load", "Initial Load of Frozen Cookies v " + FrozenCookies.branch + "." + FrozenCookies.version + ". (You should only ever see this once.)");
     if (gameSaveData) {
         FrozenCookies.loadedData = JSON.parse(gameSaveData);
     } else {
